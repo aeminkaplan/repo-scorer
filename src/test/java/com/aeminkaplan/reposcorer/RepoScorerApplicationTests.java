66084824bbd190b793c1faa48289f62831ea244f
aeminkaplan <ahmeteminkaplan@gmail.com>
2025-04-19 23:13:35 +0200

    1)Spring boot initialization 2).gitignore added

---
+++ src/test/java/com/aeminkaplan/reposcorer/RepoScorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aeminkaplan.reposcorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoScorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
