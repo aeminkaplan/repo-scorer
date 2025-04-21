29203a7396107bc813ff1f1d76cb7416f1969346
aeminkaplan <ahmeteminkaplan@gmail.com>
2025-04-21 17:50:39 +0200

    swagger added.

--- src/main/java/com/aeminkaplan/reposcorer/controller/ScorerController.java
+++ src/main/java/com/aeminkaplan/reposcorer/controller/ScorerController.java
@@ -3,6 +3,8 @@ package com.aeminkaplan.reposcorer.controller;
 import com.aeminkaplan.reposcorer.model.RepoRequest;
 import com.aeminkaplan.reposcorer.model.RepoResponse;
 import com.aeminkaplan.reposcorer.service.ScorerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -28,9 +30,14 @@ public class ScorerController {
     }
 
     @GetMapping
-    public ResponseEntity<List<RepoResponse>> getScoredRepos(@RequestParam String keyword,
-                                                             @RequestParam(required=false) String language,
-                                                             @RequestParam(required=false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDateTime earliestCreated){
+    @Operation(
+            summary = "Get scored repositories",
+            description = "Fetches and scores GitHub repositories based on stars, forks, and recent updates."
+    )
+    public ResponseEntity<List<RepoResponse>> getScoredRepos(@Parameter(description = "Keyword to search for", example = "webcrawler") @RequestParam String keyword,
+                                                             @Parameter(description = "Filter by programming language", example = "python") @RequestParam(required = false) String language,
+                                                             @Parameter(description = "Earliest created date of the repository (ISO format)", example = "2024-01-01T00:00:00")
+                                                             @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDateTime earliestCreated) {
         log.info("GET /v1/score called with keyword={}, language={}, earliestCreated={}", keyword, language, earliestCreated);
         RepoRequest request = new RepoRequest();
         request.setKeyword(keyword);
